82c9cf6586e1111e01ac0c5b5ff4a395333a7471
lekshmibala <37533079+lekshmibala@users.noreply.github.com>
2019-02-28 16:13:41 +0530

    Add files via upload

---
+++ Controllers/RegController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Registration.Controllers
+{
+    public class RegController : Controller
+    {
+        //
+        // GET: /Reg/
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Reg()
+        {
+            return View();
+        }
+	}
+}
